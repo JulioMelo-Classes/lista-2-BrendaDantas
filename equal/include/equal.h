30713ff13ed0c4aa4e4d43475eb61908a32013c5
BrendaDantas <brendasod98@gmail.com>
2021-06-26 17:58:53 +0000

    Código falhou em 2 testes.

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -16,19 +16,17 @@ namespace graal {
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
 {
- // InputIt2 iterator2 = first2;
-  //InputIt1 iterator1 = first1;
-  
   while (first1 != last1)
   {
-    if (eq(*first1, *first1))
+    if (eq(*first1, *first2))
     {      
       return true;
     }
+    return false;
+    first1++;
+    first2++;
   }
-  return false;
-  first1++;
-  first2++;    
+  
 }   
 
 template<class InputIt1, class InputIt2, class Equal>
@@ -41,11 +39,11 @@ bool equal(InputIt1 first1, InputIt1 last1,
     if (eq(*first1, *first2))
     {
       return true;      
-    }    
-  }
-  return false;
-  first1++;
-  first2++;
+    }  
+    return false;
+    first1++;
+    first2++;    
+  } 
 }
 
 }
