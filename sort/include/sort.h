7f947d48677a9bd9b2dbbff09c431327be41fe14
BrendaDantas <brendasod98@gmail.com>
2021-06-26 05:42:05 +0000

    Tarefa concluída.

--- sort/include/sort.h
+++ sort/include/sort.h
@@ -29,35 +29,5 @@ void sort(ForwardIt first, ForwardIt last, Comparison cmp)
 }
 
 }
-#endif#ifndef GRAAL_H
-#define GRAAL_H
 
-#include <utility>
-using std::pair;
-#include <iterator>
-using std::distance;
-#include <algorithm>
-using std::sort;
-
-namespace graal {
-
-/*! 
- * TODO: documentação no estilo doxygen
- */
-template<class ForwardIt, class Comparison>
-void sort(ForwardIt first, ForwardIt last, Comparison cmp)
-{  
-  for(ForwardIt iterator = first; iterator != last; iterator++)
-  {
-    for(ForwardIt iterator2 = iterator; iterator2 != last; iterator2++)
-    {
-      if(cmp(*iterator2,*iterator))
-      {
-        std::iter_swap(iterator,iterator2);
-      }
-    }
-  }
-}
-
-}
 #endif
