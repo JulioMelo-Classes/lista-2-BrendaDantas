c451e574a44155357b12ddc8ac99fc97926bf173
BrendaDantas <brendasod98@gmail.com>
2021-06-26 14:49:05 +0000

    Essa tua lógica tá foda, tente outras antes.

--- rotate/include/rotate.h
+++ rotate/include/rotate.h
@@ -16,8 +16,30 @@ namespace graal {
 template<class ForwardIt>
 ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last)
 {
-    // TODO
-   return last;
+   ForwardIt auxiliar = first; 
+   ForwardIt auxiliar2 = first;
+   ForwardIt auxiliar3 = first;
+   ForwardIt iterator = first+1;
+   ForwardIt guardar_n_first = n_first;
+   ForwardIt guardar_n_first_anterior = n_first-1;
+
+   while (*first != *guardar_n_first) 
+   {
+     auxiliar2 = auxiliar3;
+     auxiliar3 = iterator;
+     auxiliar3++;
+     iterator++;     
+
+     if (iterator == (last-1)) 
+     {
+       auxiliar3 = iterator;
+       iterator = auxiliar2;
+       iterator = first+1;  
+       auxiliar3 = first;     
+       auxiliar = auxiliar3;
+     }
+   }
+   //return guardar_n_first_anterior;
 }
 
 }
