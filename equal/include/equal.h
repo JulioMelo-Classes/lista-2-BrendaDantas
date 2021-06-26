4e0b05a9ea2507232ca80ec47888c6627736418a
BrendaDantas <brendasod98@gmail.com>
2021-06-26 19:02:11 +0000

    Código falhou em 1 teste.

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -18,15 +18,18 @@ bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
 {
   while (first1 != last1)
   {
+    if (first1 > first2) 
+    {
+      break;
+    }
     if (eq(*first1, *first2))
     {      
       return true;
     }
     return false;
     first1++;
-    first2++;
+    first2++; 
   }
-  
 }   
 
 template<class InputIt1, class InputIt2, class Equal>
@@ -34,15 +37,19 @@ bool equal(InputIt1 first1, InputIt1 last1,
            InputIt2 first2, InputIt2 last2,
            Equal eq )
 {
-  while (first1 != last1)
+  while (first1 != last2)
   {
+    if (first1 > first2) 
+    {
+      break;
+    }
     if (eq(*first1, *first2))
     {
-      return true;      
-    }  
+      return true;
+    }
     return false;
     first1++;
-    first2++;    
+    first2++;
   } 
 }
 
