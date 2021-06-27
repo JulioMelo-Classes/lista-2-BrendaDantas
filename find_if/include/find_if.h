2ff5d460dd64ae69425929ea79917b65de4ccae8
BrendaDantas <brendasod98@gmail.com>
2021-06-27 14:19:41 +0000

    Tarefa concluída e documentada.

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -11,13 +11,16 @@ using std::sort;
 namespace graal {
 
 /*! 
- * @brief Function that receives a range verify if each element of the range satisfy a predicate.
+* @brief Function that receives a range verify if each element of the range satisfy a predicate.
+ *
+ * @tparam InputIt  iterator to the range.
+ * @tparam UnaryPredicate iterator to the predicate.
+ *
+ * @param first pointer to the first element of the range.
+ * @param last pointer to the position just after the last element of the range.
+ *
+ * @return iterator to the first element that satisfy the predicate and if all the elements in the range doesn't satisfy the predicate, return last.
 
-  * @tparam InputIt = iterator to the range.
-  * @param UnaryPredicate = iterator to the predicate.
-  * @param first = pointer that points to the first element of the range.
-  * @param last = pointer that points to the position after the last element.
-  * @return iterator to the first element that satisfy the predicate and if all the elements in the range doesn't satisfy the predicate, return last.
  */ 
 template<class InputIt, class UnaryPredicate>
 InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
