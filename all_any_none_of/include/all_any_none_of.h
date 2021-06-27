71cddea55582351dc22ac64c0bcb9b03ad215e34
BrendaDantas <brendaod98@gmail.com>
2021-06-27 03:39:07 +0000

    Tarefa concluída e documentada.

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -10,8 +10,15 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+ /*! 
+  * @brief Function that receive a range and a predicate and verify if the predicate is being satisfied for all the elements.
+
+  * @tparam InputIt = iterator to the range.
+  * @tparam UnaryPredicate = iterator to the predicate.
+  * @param first = pointer that points to the first element of the range.
+  * @param last = pointer that points to the position after the last element.
+  * @param iterator = iterator that runs through all the elements of the range.
+  * @return true when the predicate is satisfied and false when this doesn't happens.
  */
 template<class InputIt, class UnaryPredicate>
 bool all_of(InputIt first, InputIt last, UnaryPredicate p)
@@ -28,7 +35,14 @@ bool all_of(InputIt first, InputIt last, UnaryPredicate p)
 }
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @brief Function that receive a range and a predicate and verify if the predicate it's being satisfied for at least one element of the range.
+
+  * @tparam InputIt = iterator to the range.
+  * @tparam UnaryPredicate = iterator to the predicate.
+  * @param first = pointer that points to the first element of the range.
+  * @param last = pointer that points to the position after the last element.
+  * @param iterator = iterator that runs through all the elements of the range.
+  * @return true when the predicate is satisfied and false when this doesn't happens.
  */
 template<class InputIt, class UnaryPredicate>
 bool any_of(InputIt first, InputIt last, UnaryPredicate p)
@@ -44,7 +58,14 @@ bool any_of(InputIt first, InputIt last, UnaryPredicate p)
 }
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @brief Function that receive a range and a predicate and verify if the predicate it it not being satisfied for all the elements of the range.
+
+  * @tparam InputIt = iterator to the range.
+  * @tparam UnaryPredicate = iterator to the predicate.
+  * @param first = pointer that points to the first element of the range.
+  * @param last = pointer that points to the position after the last element.
+  * @param iterator = iterator that runs through all the elements of the range.
+  * @return false when the predicate is being satisfied and true if it's not.
  */
 template<class InputIt, class UnaryPredicate>
 bool none_of(InputIt first, InputIt last, UnaryPredicate p)
