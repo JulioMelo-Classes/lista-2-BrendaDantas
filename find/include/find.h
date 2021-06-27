aeca7c03234302fdcc0b50c7c8ce0f300d7211ff
BrendaDantas <brendaod98@gmail.com>
2021-06-27 03:53:33 +0000

    Tarefa concluída e documentada.

--- find/include/find.h
+++ find/include/find.h
@@ -11,7 +11,14 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @brief Function that receives a range, a value and returns an iterator to the first element on the range that is equal to the value received.
+
+  * @tparam InputIt = iterator to the range.
+  * @param first = pointer that points to the first element of the range.
+  * @param last = pointer that points to the position after the last element.
+  * @param value = a value that is received by parameter.
+  * @param Equal = function that verify if two elements are equal to each others.
+  * @return iterator if two values are equal, and return last if they are not equal.
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
