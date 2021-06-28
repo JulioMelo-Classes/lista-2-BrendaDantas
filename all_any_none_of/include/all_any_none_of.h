27b700d6e6d62e8850d4973ca6cb63e9929aa11c
brendaDantas <brendasod98@gmail.com>
2021-06-28 01:28:22 +0000

    Tarefa concluída e documentada.

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -11,14 +11,17 @@ using std::sort;
 namespace graal {
 
  /*! 
-  * @brief Function that receive a range and a predicate and verify if the predicate is being satisfied for all the elements.
 
-  * @tparam InputIt = iterator to the range.
-  * @tparam UnaryPredicate = iterator to the predicate.
-  * @param first = pointer that points to the first element of the range.
-  * @param last = pointer that points to the position after the last element.
-  * @param iterator = iterator that runs through all the elements of the range.
+  *@brief Function that receives a range and a predicate and verify if the predicate is being satisfied for all the elements.
+  *
+  * @tparam InputIt iterator to the range.
+  * @tparam UnaryPredicate iterator to the predicate.
+  *
+  * @param first pointer to the first element of the range.
+  * @param last pointer to the position just after the last element of the range.
+  *
   * @return true when the predicate is satisfied and false when this doesn't happens.
+
  */
 template<class InputIt, class UnaryPredicate>
 bool all_of(InputIt first, InputIt last, UnaryPredicate p)
@@ -35,13 +38,14 @@ bool all_of(InputIt first, InputIt last, UnaryPredicate p)
 }
 
 /*! 
- * @brief Function that receive a range and a predicate and verify if the predicate it's being satisfied for at least one element of the range.
-
-  * @tparam InputIt = iterator to the range.
-  * @tparam UnaryPredicate = iterator to the predicate.
-  * @param first = pointer that points to the first element of the range.
-  * @param last = pointer that points to the position after the last element.
-  * @param iterator = iterator that runs through all the elements of the range.
+  *@brief Function that receives a range and a predicate and verify if the predicate it's being satisfied for at least one element of the range.
+  *
+  * @tparam InputIt iterator to the range.
+  * @tparam UnaryPredicate iterator to the predicate.
+  *
+  * @param first pointer to the first element of the range.
+  * @param last pointer to the position just after the last element of the range.
+  *
   * @return true when the predicate is satisfied and false when this doesn't happens.
  */
 template<class InputIt, class UnaryPredicate>
@@ -58,13 +62,14 @@ bool any_of(InputIt first, InputIt last, UnaryPredicate p)
 }
 
 /*! 
- * @brief Function that receive a range and a predicate and verify if the predicate it it not being satisfied for all the elements of the range.
-
-  * @tparam InputIt = iterator to the range.
-  * @tparam UnaryPredicate = iterator to the predicate.
-  * @param first = pointer that points to the first element of the range.
-  * @param last = pointer that points to the position after the last element.
-  * @param iterator = iterator that runs through all the elements of the range.
+  *@brief Function that receives a range and a predicate and verify if the predicate it it not being satisfied for all the elements of the range.
+  *
+  * @tparam InputIt iterator to the range.
+  * @tparam UnaryPredicate iterator to the predicate.
+  *
+  * @param first pointer to the first element of the range.
+  * @param last pointer to the position just after the last element of the range.
+  *
   * @return false when the predicate is being satisfied and true if it's not.
  */
 template<class InputIt, class UnaryPredicate>
